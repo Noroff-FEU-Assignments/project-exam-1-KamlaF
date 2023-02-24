586286494d1f3e05fea26826ac59c9ee11ed23f8
KamlaF <100962390+KamlaF@users.noreply.github.com>
2023-02-24 23:01:42 +0100

    update 24/2

--- Home.js
+++ Home.js
@@ -1,6 +1,5 @@
 baseUrl = "https://kamillafagerland.com/blogexam/wp-json/wp/v2/posts?_embed";
-const carouselContainer = document.querySelector(".carouselContainer");
-const loadMore = document.querySelector(".loadMore");
+const imagesSlideshow = document.querySelector(".wrapper");
 
 
 
@@ -8,46 +7,74 @@ async function getProducts(url){
   const response = await fetch(url);
   const posts = await response.json();
 
-  carouselContainer.innerHTML = "";
+  imagesSlideshow.innerHTML = "";
   
   posts.forEach(function(posts){
     console.log(posts);
 
-  
-    carouselContainer.innerHTML +=`
     
     
-    <a href="/blogPost.html?id=${posts.id}" class="wrapper">
-           <img class="carousel-image" style="background-image:url('${posts._embedded['wp:featuredmedia'][0].source_url}')"></img>
-          
-    </a> `
+    
+    imagesSlideshow.innerHTML +=`
+
+    <a href="/blogPost.html?id=${posts.id}" class="carousel">
+               <img class="carousel-image2" draggable="false" style="background-image:url('${posts._embedded['wp:featuredmedia'][0].source_url}')">
+              
+               </a>   `
+              
   })
+  
+  const carousel = document.querySelector(".wrapper");
+  firstImg = carousel.querySelectorAll(".carousel-image2")[0];
+  arrowIcons = document.querySelectorAll(".carouselBody i");
 
-  const carousel = document.querySelector(".carouselContainer");
-  firstImg = carouselContainer.querySelectorAll("img")[0];
-  arrowIcons= document.querySelectorAll(".fa-angle-right, .fa-angle-left");
+  let isDragStart = false, prevPageX, prevScrollLeft;
+  let firstImgWidth = firstImg.clientWidth;
 
-  let isDragStart = false;
-  let firstImgWidth = firstImg.clientWidth 
-
-  arrowIcons.forEach(icon =>{
+  arrowIcons.forEach(icon => {
     icon.addEventListener("click", () =>{
       carousel.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
-       
-    });
 
-  })
+    })
 
-  const dragging =(e)=>{
-    carousel.scrollLeft=e.pageX;
+  });
+
+  const dragStart = (e) => {
+    isDragStart = true;
+    prevPageX = e.pageX || e.thouches[0].pageX ;
+    prevScrollLeft = carousel.scrollLeft;
+
+
   }
+
+  const dragging = (e) => {
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.classList.add("dragging");
+    let positionDiff = (e.pageX || e.thouches[0].pageX) - prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+  }
+
+  const dragStop = () => {
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+  }
+
+  carousel.addEventListener("mousedown", dragStart);
+  carousel.addEventListener("thouchstart", dragStart);
+
   carousel.addEventListener("mouseover", dragging);
+  carousel.addEventListener("thouchmove", dragStart);
+
+  carousel.addEventListener("mouseup", dragStop);
+  carousel.addEventListener("mouseleave", dragStop);
+  carousel.addEventListener("thouchend", dragStart);
+  
+
+
 }
 
+  
 getProducts(baseUrl);
-
-
-
-
 
 
